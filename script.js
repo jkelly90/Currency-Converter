771f58a1bc3c802984c4651fd8e82009a04ddbfc
jkelly90 <jlfkelly@gmail.com>
2020-06-03 18:22:40 +0200

    added api

---
+++ script.js
@@ -0,0 +1,28 @@
+const currencyEl_one = document.getElementById('currency-one');
+const amountEl_one = document.getElementById('amount-one');
+const currencyEl_two = document.getElementById('currency-two');
+const amountEl_two = document.getElementById('amount-two');
+
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+// Fetch exchange rates and update the DOM
+function calculate() {
+    const currency_one = currencyEl_one.value;
+    const currency_two = currencyEl_two.value;
+
+    fetch(`https://v6.exchangerate-api.com/v6/36f8257b40c7cdefadeb8db2/latest/USD`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+        })
+}
+
+
+// Event Listeners
+currencyEl_one.addEventListener('change', calculate);
+amountEl_one.addEventListener('input', calculate);
+currencyEl_two.addEventListener('change', calculate);
+amountEl_two.addEventListener('input', calculate);
+
+calculate();
